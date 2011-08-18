85abafd816e8ad6db7e03f008999f572d737d5b7
jeromerodrigo <jeromerodrigo@gmail.com>
2011-08-19 00:06:23 +0800

    Base copy - Done Login.aspx

---
+++ Login.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Log In - XALSA</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Login Page<br />
+        <br />
+        Username:
+        <asp:TextBox ID="Username" runat="server" MaxLength="20"></asp:TextBox>
+&nbsp;<asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+            ControlToValidate="Username" Display="Dynamic" ErrorMessage="Insert Username"></asp:RequiredFieldValidator><br />
+        <br />
+        Password:
+        <asp:TextBox ID="Password" runat="server" MaxLength="20" TextMode="Password"></asp:TextBox>
+&nbsp;<asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" 
+            ControlToValidate="Password" Display="Dynamic" ErrorMessage="Insert Password"></asp:RequiredFieldValidator><br />
+        <br />
+        <asp:Button ID="LoginButton" runat="server" onclick="LoginButton_Click" 
+            Text="Log In" />
+&nbsp;<asp:Label ID="Label1" runat="server"></asp:Label><br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void LoginButton_Click(object sender, EventArgs e)
+    {
+        AccessDataSource ads = new AccessDataSource();
+        ads.DataFile = "~/App_Data/Xalsa.mdb";
+
+        if (this.Username.Text.Length == 0 | this.Password.Text.Length == 0)
+        {
+            this.Label1.ForeColor = System.Drawing.Color.Red;
+            this.Label1.Text = "Login failed!";
+            this.Username.Text = "";
+            this.Password.Text = "";
+            return;
+        }
+
+        ads.SelectParameters.Add("Username", this.Username.Text);
+        ads.SelectParameters.Add("Password", this.Password.Text);
+
+        ads.SelectCommand = "SELECT * FROM [Login] WHERE [Username] = @Username AND [Password] = @Password";
+
+        DataView dv = (DataView)ads.Select(DataSourceSelectArguments.Empty);
+
+        if (dv.Count == 0)
+        {
+            this.Label1.ForeColor = System.Drawing.Color.Red;
+            this.Label1.Text = "Account details does not exist. Please register.";
+            this.Username.Text = "";
+            this.Password.Text = "";
+            return;
+        }
+
+        this.Session["Username"] = dv[0].Row["Username"].ToString();
+        this.Session["UserType"] = dv[0].Row["UserType"].ToString();
+
+        if (this.Session["UserType"].ToString().Equals("member"))
+            Response.Redirect("MembersHome.aspx");
+        else if (this.Session["UserType"].ToString().Equals("admin"))
+            Response.Redirect("AdminHome.aspx");
+    }
+}
